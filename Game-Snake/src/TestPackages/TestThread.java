bb65a46462879a97badfc05cc79dd310344b9384
Huu Hien <32191787+huuhien1747@users.noreply.github.com>
2018-07-25 16:38:30 +0700

    no message

--- Game-Snake/src/TestPackages/TestThread.java
+++ Game-Snake/src/TestPackages/TestThread.java
@@ -5,21 +5,44 @@
  */
 package TestPackages;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Huu Hien
  */
 public class TestThread extends Thread {
-    public TestThread(){
-        this.start();
+    public String caller;
+    public TestThread(String caller){
+        this.caller = caller;
     }
     
     public static void main(String[] args) {
-        
+        TestThread testStart = new TestThread("start");
+        TestThread testRun = new TestThread("run");
+        testStart.start();
+        //testStart.start();
+        //testRun.run();
     }
     
     @Override
-    public void run(){
+    public void run(){   
+        System.out.println("Caller: "+ this.caller + 
+                " and code on this Thread is exeuted by : " + 
+                Thread.currentThread().getName());   
+ //       /*
+        int i= 0;
+        do{
+            try {
+            this.sleep(2000);
+            i+= 2;
+            } catch (InterruptedException ex) {
+                Logger.getLogger(TestThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            System.out.println("Thread sleep in 2s!, counting: " +i);
+        }while(i <100);
         
+//        */
     }
 }
