f22d5b72c2c44bd71440d5434ba4ed8dcdbaba2d
Huu Hien <32191787+huuhien1747@users.noreply.github.com>
2018-07-25 15:29:47 +0700

    Write First Code

---
+++ Game-Snake/src/TestPackages/TestThread.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestPackages;
+
+/**
+ *
+ * @author Huu Hien
+ */
+public class TestThread extends Thread {
+    public TestThread(){
+        this.start();
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    @Override
+    public void run(){
+        
+    }
+}
